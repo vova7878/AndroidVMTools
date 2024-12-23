fef337d61182bff4958beb456c315f3dd7380810
v7878 <46866705+vova7878@users.noreply.github.com>
2024-12-23 19:46:46 +0700

    Fix EntryPoints.getGenericJniTrampoline for some devices

--- src/main/java/com/v7878/vmtools/EntryPoints.java
+++ src/main/java/com/v7878/vmtools/EntryPoints.java
@@ -1,32 +1,59 @@
 package com.v7878.vmtools;
 
+import static com.v7878.dex.DexConstants.ACC_ABSTRACT;
+import static com.v7878.dex.DexConstants.ACC_NATIVE;
+import static com.v7878.dex.DexConstants.ACC_PRIVATE;
+import static com.v7878.dex.DexConstants.ACC_PUBLIC;
 import static com.v7878.unsafe.ArtMethodUtils.getExecutableEntryPoint;
 import static com.v7878.unsafe.Reflection.getDeclaredMethod;
 
-import com.v7878.r8.annotations.DoNotObfuscate;
-import com.v7878.r8.annotations.DoNotShrink;
-import com.v7878.r8.annotations.DoNotShrinkType;
-import com.v7878.r8.annotations.KeepCodeAttribute;
+import com.v7878.dex.ClassDef;
+import com.v7878.dex.Dex;
+import com.v7878.dex.EncodedMethod;
+import com.v7878.dex.MethodId;
+import com.v7878.dex.ProtoId;
+import com.v7878.dex.TypeId;
+import com.v7878.unsafe.DexFileUtils;
+
+import java.lang.invoke.MethodType;
 
 public class EntryPoints {
-    @SuppressWarnings("unused")
-    @DoNotShrinkType
-    private static abstract class Test {
-        @DoNotShrink
-        @DoNotObfuscate
-        @KeepCodeAttribute
-        abstract void interpreter();
+    private static final long generic_jni_trampoline;
+    private static final long to_interpreter_bridge;
 
-        @DoNotShrink
-        @DoNotObfuscate
-        @KeepCodeAttribute
-        native void jni();
+    // Art can cache trampolines for common native call types, so we have to do something uncommon
+    private static final MethodType JNI_TYPE = MethodType.methodType(Object.class,
+            Object.class, int.class, long.class, float.class, double.class,
+            Object.class, int.class, long.class, float.class, double.class,
+            Object.class, int.class, long.class, float.class, double.class,
+            Object.class, int.class, long.class, float.class, double.class,
+            Object.class, int.class, long.class, float.class, double.class,
+            Object.class, int.class, long.class, float.class, double.class);
+
+    static {
+        TypeId obj_id = TypeId.OBJECT;
+
+        String test_name = EntryPoints.class.getName() + "$Test";
+        TypeId test_id = TypeId.of(test_name);
+        ClassDef test_def = new ClassDef(test_id);
+        test_def.setSuperClass(obj_id);
+
+        MethodId jni_method_id = new MethodId(test_id, ProtoId.of(JNI_TYPE), "jni");
+        test_def.getClassData().getDirectMethods().add(
+                new EncodedMethod(jni_method_id, ACC_PRIVATE | ACC_NATIVE));
+
+        MethodId interpreter_method_id = new MethodId(test_id, new ProtoId(TypeId.V), "interpreter");
+        test_def.getClassData().getVirtualMethods().add(
+                new EncodedMethod(interpreter_method_id, ACC_PUBLIC | ACC_ABSTRACT));
+
+        var dexfile = DexFileUtils.openDexFile(new Dex(test_def).compile());
+        var clazz = DexFileUtils.loadClass(dexfile, test_name, EntryPoints.class.getClassLoader());
+
+        generic_jni_trampoline = getExecutableEntryPoint(
+                getDeclaredMethod(clazz, "jni", JNI_TYPE.parameterArray()));
+        to_interpreter_bridge = getExecutableEntryPoint(
+                getDeclaredMethod(clazz, "interpreter"));
     }
-
-    private static final long generic_jni_trampoline = getExecutableEntryPoint(
-            getDeclaredMethod(Test.class, "jni"));
-    private static final long to_interpreter_bridge = getExecutableEntryPoint(
-            getDeclaredMethod(Test.class, "interpreter"));
 
     public static long getGenericJniTrampoline() {
         return generic_jni_trampoline;
